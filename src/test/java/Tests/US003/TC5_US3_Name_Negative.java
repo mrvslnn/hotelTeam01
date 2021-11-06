a0c7977bad8a919dba69f3549a2451f97c60bba8
Ahmet Öztürk <caretaker@gmail.com>
2021-11-06 16:03:07 +0300

    commit01

--- src/test/java/Tests/US003/TC5_US3_Name_Negative.java
+++ src/test/java/Tests/US003/TC5_US3_Name_Negative.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.US003_Page.QAConcortPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
